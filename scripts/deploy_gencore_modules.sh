6cb24590efe0e9cb999dcb5e91eff93b22aa027d
jerowe <jillian.e.rowe@gmail.com>
2017-06-15 10:50:46 +0400

    adding in some scripts

--- scripts/deploy_gencore_modules.sh
+++ scripts/deploy_gencore_modules.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-### NYUAD CGSB EasyBuild Module Deployment 
+### NYUAD CGSB EasyBuild Module Deployment
 ##################################################################
-## Base Image Setup 
+## Base Image Setup
 ##################################################################
 
 #sudo yum install -y git wget tar make bzip2 patch which
@@ -13,12 +13,11 @@
 ## We are using miniconda3/python3 from anaconda
 ## Be sure to change this if you are running as a your own user
 ##################################################################
-export ANACONDA3_BASE=/home/jillian/.local/anaconda3 
+export ANACONDA3_BASE=/home/jillian/.local/anaconda3
 
 curl -s -L https://repo.continuum.io/miniconda/Miniconda3-4.2.12-Linux-x86_64.sh  > miniconda.sh && \
     openssl md5 miniconda.sh | grep d0c7c71cc5659e54ab51f2005a8d96f3
 
-mkdir -p /opt/
 chmod 777 miniconda.sh ; bash miniconda.sh -b -p $ANACONDA3_BASE 
 rm miniconda.sh && \
     export PATH=${ANACONDA3_BASE}/bin:$PATH && \
@@ -29,9 +28,9 @@ rm miniconda.sh && \
     conda clean -tipy
 
 ##################################################################
-## EasyBuild User Setup 
+## EasyBuild User Setup
 ## This is only necessary if you are creating a user for EB
-## If running as your own user skip this section 
+## If running as your own user skip this section
 ## But you cannot run easybuild as root
 ##################################################################
 #useradd -ms /bin/bash ebuser
@@ -41,8 +40,8 @@ rm miniconda.sh && \
 #export HOME=/home/ebuser
 #
 ##################################################################
-## EasyBuild User Setup 
-## This is only necessary if you 
+## EasyBuild User Setup
+## This is only necessary if you
 ##################################################################
 
 export PATH=${ANACONDA3_BASE}/bin:$PATH
@@ -51,18 +50,18 @@ export EB_ENV=$HOME/.eb/eb--3.1.10_lmod--7.3.16
 export MODULEPATH=$EASYBUILD_PREFIX/modules/all
 
 ##################################################################
-## Add Easybuild Config 
+## Add Easybuild Config
 ## For now the config doesn't have anything except some commented
-## out sections for the modules-tool and modules-syntax 
+## out sections for the modules-tool and modules-syntax
 ##################################################################
 mkdir -p $HOME/.config/easybuild
- 
+
 ##################################################################
 ## Conda config for ebuser
-## Setup the initial conda configuration for the ebuser 
+## Setup the initial conda configuration for the ebuser
 ##################################################################
 conda config --set show_channel_urls True && \
-    conda config --add channels nyuad-cgsb && \ 
+    conda config --add channels nyuad-cgsb && \
     conda config --add channels conda-forge && \
     conda config --add channels defaults && \
     conda config --add channels r && \
@@ -73,32 +72,32 @@ conda config --set show_channel_urls True && \
 
 ##################################################################
 ## Install Easybuild and Lmod
-## Create a conda env with easybuild and lmod - and nothing else  
+## Create a conda env with easybuild and lmod - and nothing else
 ##################################################################
-conda create -p $EB_ENV easybuild lmod 
+conda create -p $EB_ENV easybuild lmod
 
 ##################################################################
-## Install an Easybuild Config 
-## Anaconda2 is a Core EB config  
-## --software-name is a way of telling EB to look for software 
-## from the repos instead of supplying a .eb file 
+## Install an Easybuild Config
+## Anaconda2 is a Core EB config
+## --software-name is a way of telling EB to look for software
+## from the repos instead of supplying a .eb file
 ##################################################################
 source activate $EB_ENV && \
-	eb --software-name Anaconda2 
+	eb --software-name Anaconda2
 
 ##################################################################
-## Add NYUAD Easybuild Configs 
-## These are custom configs outside of the Easybuild Main  
+## Add NYUAD Easybuild Configs
+## These are custom configs outside of the Easybuild Main
 ##################################################################
 mkdir -p $HOME/.eb/custom_repos
-cd $HOME/.eb/custom_repos 
+cd $HOME/.eb/custom_repos
 git clone https://github.com/nyuad-cgsb/nyuad-hpc-module-configs
-cd $HOME 
+cd $HOME
 
 ###################################################################
-### Add NYUAD Easybuild Configs to the Robot Path 
+### Add NYUAD Easybuild Configs to the Robot Path
 ### Robot will tell EB to automatically pull in deps
-### Robot-path will tell EB where to look for configs  
+### Robot-path will tell EB where to look for configs
 ###################################################################
 export ROBOT=$HOME/.eb/custom_repos/nyuad-hpc-module-configs/_easybuild
 
@@ -112,11 +111,10 @@ eb --extended-dry-run  --robot --robot-paths=$ROBOT  gencore_rnaseq-1.0.eb
 module avail
 
 ##################################################################
-## Install a gnecore module 
+## Install a gnecore module
 ## Finally, running this command will install the gencore_rnaseq-1.0 module
 ## Additionally, it will install the gencore_anaconda2 module if it is not already available
 ##################################################################
 
 # eb  --robot --robot-paths=$ROBOT  gencore_rnaseq-1.0.eb
 # module avail
-
