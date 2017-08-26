5cb14f34f8822bd6f5339fe40ac14515a79e6816
jerowe <jillian.e.rowe@gmail.com>
2017-08-26 09:07:22 +0400

    auth still not working

--- scripts/build_easybuild.sh
+++ scripts/build_easybuild.sh
@@ -2,26 +2,23 @@
 
 set -ex
 
+echo "BUILDING EASYBUILD"
+
 if [[ -z "${GITHUB_TOKEN}" ]] ; then
     echo "GitHub API key needs to be set to update docs."
     exit 0
 fi
-
-# Build the documentation
-#GITHUB_USERNAME=${TRAVIS_REPO_SLUG%/*}
 
 cd /nyuad-conda-configs
 
 #At least we can test if this works
 mkdir -p _easybuild
 
-echo "BUILDING EASYBUILD"
-
 git status
 git add -A
 
-git config  user.email "nobody@nobody.org"
-git config  user.name "Travis CI"
+git config  user.email "jillian.e.rowe@gmail.com"
+git config  user.name "jerowe"
 
 ORIGIN="https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git"
 
@@ -29,9 +26,9 @@ git remote rm origin
 git remote add origin "$ORIGIN"
 
 git add -A
-git checkout "$TRAVIS_BRANCH"
+git pull origin "$TRAVIS_BRANCH"
 
 git add _easybuild
 #If it doesn't exit as 0 there is nothing to commit
 git commit  -m "Updated docs to commit ${TRAVIS_COMMIT}." || exit 0
-git push -f "$ORIGIN" "$TRAVIS_BRANCH"
+git push  origin "$TRAVIS_BRANCH"
