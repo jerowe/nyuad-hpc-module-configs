f1e0beff5e1467cc709148964ae00adc81449522
jerowe <jillian.e.rowe@gmail.com>
2018-01-02 14:46:24 +0400

    try it with new build system

--- tests/travis-run.sh
+++ tests/travis-run.sh
@@ -1,15 +1,62 @@
 #!/bin/bash
 
-set -euo pipefail
+# set -euo pipefail
+#
+# if [[ $TRAVIS_OS_NAME = "linux" ]]
+# then
+#
+#     docker run -e TRAVIS_PULL_REQUEST -e TRAVIS_BRANCH \
+#         -e GITHUB_TOKEN -e GITHUB_USER -e GITHUB_REPO \
+#         -e ANACONDA_TOKEN -e ANACONDA_PASSWORD -e ANACONDA_USER \
+#         -i -t -v `pwd`:/nyuad-conda-configs quay.io/nyuad_cgsb/anaconda-centos /nyuad-conda-configs/tests/run-tests.sh
+#
+# else
+#     exit 0
+# fi
+set -xe # Exit with nonzero exit code if anything fails
 
-if [[ $TRAVIS_OS_NAME = "linux" ]]
+export BASE_DIR=`pwd`
+
+#git fetch origin master
+#export RECIPES=$(git diff FETCH_HEAD --name-only | grep yml | grep recipes)
+#
+#echo "Processing recipes..."
+#echo $RECIPES
+#echo ""
+
+if [[ $TRAVIS_BRANCH = "master" && "$TRAVIS_PULL_REQUEST" = false ]]
 then
+    #Upload packages
+    anaconda login --user $ANACONDA_USER --password $ANACONDA_PASSWORD
+    conda config --set anaconda_upload yes
 
-    docker run -e TRAVIS_PULL_REQUEST -e TRAVIS_BRANCH \
-        -e GITHUB_TOKEN -e GITHUB_USER -e GITHUB_REPO \
-        -e ANACONDA_TOKEN -e ANACONDA_PASSWORD -e ANACONDA_USER \
-        -i -t -v `pwd`:/nyuad-conda-configs quay.io/nyuad_cgsb/anaconda-centos /nyuad-conda-configs/tests/run-tests.sh
+    echo "Gencore App build ebs"
+    # cd /nyuad-conda-configs
+    cd $BASE_DIR
+    gencore_app build_eb
 
+    echo "Gencore App building docs"
+    # cd /nyuad-conda-configs
+    cd $BASE_DIR
+    gencore_app build_docs
+
+    echo "Commit docs to github"
+    # cd /nyuad-conda-configs
+    cd $BASE_DIR
+    scripts/build_easybuild.sh
+    scripts/build_docs.sh
+
+    echo "Building man pages!"
+    # cd /nyuad-conda-configs
+    cd $BASE_DIR
+    gencore_app build_man
+
+    echo "Uploading packages to anaconda!"
+    gencore_app upload_envs
+
+    scripts/commit_recipes.sh
 else
-    exit 0
+    #Just test packages
+    # gencore_app build_envs
+    git diff FETCH_HEAD --name-only | grep yml | grep recipes | xargs -I {} gencore_app build_envs -e {}
 fi
--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 set -e
 
-if [[ $TRAVIS_OS_NAME = "linux" ]]
-then
-    docker pull quay.io/nyuad_cgsb/anaconda-centos
-else
+# if [[ $TRAVIS_OS_NAME = "linux" ]]
+# then
+#     docker pull quay.io/nyuad_cgsb/anaconda-centos
+# else
+#
+#     exit 0
+# fi
 
-    exit 0
-fi
+pip install pyyaml
+
+sudo chown -R $USER /opt/anaconda3
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --quiet -O miniconda.sh
+bash miniconda.sh -b -p /home/travis/anaconda3
+
+export PATH="/home/travis/anaconda3/bin:$PATH"
+
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels r
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda config --add channels nyuad-cgsb
+
+# conda index /home/travis/anaconda3/conda-bld/linux-64
+# conda config --add channels file://home/travis/anaconda3/conda-bld
+conda update --all -y
+
+conda install conda conda-build
+conda install -y r-base r-essentials openjdk perl bioconductor-biobase nodejs
+npm install -g marked-man
+
+conda install -y pip
+pip install git+https://github.com/nyuad-cgsb/gencore_app.git@master
