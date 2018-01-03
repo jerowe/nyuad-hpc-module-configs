58f57517ec740678b753686019937e3908cb0c43
jerowe <jillian.e.rowe@gmail.com>
2018-01-03 16:48:32 +0400

    try again

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -35,5 +35,5 @@ conda install -y samtools bamtools tophat bcftools freebayes gatk tophat
 npm install -g marked-man
 
 conda install -y pip
-/home/travis/anaconda3/bin/pip remove gencore_app || echo "Gencore app is not installed"
+/home/travis/anaconda3/bin/pip uninstall gencore_app || echo "Gencore app is not installed"
 /home/travis/anaconda3/bin/pip install git+https://github.com/nyuad-cgsb/gencore_app.git@master
