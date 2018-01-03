567dea4838f34ff15ad599e4098322b7d7b97a2c
jerowe <jillian.e.rowe@gmail.com>
2018-01-03 11:19:21 +0400

    not sure why this is timing out

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -58,8 +58,8 @@ then
     scripts/commit_recipes.sh
 else
     #Just test packages
-    python --version
-    gencore_app --help
     gencore_app build_envs
     # git diff FETCH_HEAD --name-only | grep yml | grep recipes | xargs -I {} gencore_app build_envs -e {}
 fi
+
+exit 0
