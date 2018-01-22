557dca1084606e4b8c545068b37018349f94c35b
jerowe <jillian.e.rowe@gmail.com>
2018-01-22 12:05:49 +0400

    test on circle ci

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -11,12 +11,12 @@ set -e
 
 # pip install pyyaml
 
-sudo chown -R $USER /opt/anaconda3
+# sudo chown -R $USER /opt/anaconda3
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --quiet -O miniconda.sh
-bash miniconda.sh -b -p /home/travis/anaconda3
-
-export PATH="/home/travis/anaconda3/bin:$PATH"
+# wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --quiet -O miniconda.sh
+# bash miniconda.sh -b -p $HOME/anaconda3
+#
+# export PATH="$HOME/anaconda3/bin:$PATH"
 
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels r
