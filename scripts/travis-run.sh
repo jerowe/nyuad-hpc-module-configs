92010afded68c65ba4c5ca1bd17096cc716400c6
jerowe <jillian.e.rowe@gmail.com>
2018-01-24 11:01:42 +0400

    got rid of docker things in circle ci config

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# set -euo pipefail
-#
-# if [[ $TRAVIS_OS_NAME = "linux" ]]
-# then
-#
-#     docker run -e TRAVIS_PULL_REQUEST -e TRAVIS_BRANCH \
-#         -e GITHUB_TOKEN -e GITHUB_USER -e GITHUB_REPO \
-#         -e ANACONDA_TOKEN -e ANACONDA_PASSWORD -e ANACONDA_USER \
-#         -i -t -v `pwd`:/nyuad-conda-configs quay.io/nyuad_cgsb/anaconda-centos /nyuad-conda-configs/tests/run-tests.sh
-#
-# else
-#     exit 0
-# fi
 set -xe # Exit with nonzero exit code if anything fails
 
 export BASE_DIR=`pwd`
