be928560e5a2117aa104aa6eb84cc18e4996d3d8
jerowe <jillian.e.rowe@gmail.com>
2018-05-21 11:39:19 +0400

    lets try this again

--- package_template/fetch_and_run.sh
+++ package_template/fetch_and_run.sh
@@ -4,13 +4,15 @@ set -x -e
 
 echo `pwd`
 echo "hello from fetch and run..."
+#
+#SYNC_DIR=$1
+#RUN=$2
+#
+#mkdir -p `basename $1`
+#aws s3 sync $1 hpc-runner/`basename $1`
+#chmod 777 $RUN
+#bash $RUN
+#
+#ls -lah `basename $1`
 
-SYNC_DIR=$1
-RUN=$2
-
-mkdir -p `basename $1`
-aws s3 sync $1 hpc-runner/`basename $1`
-chmod 777 $RUN
-bash $RUN
-
-ls -lah `basename $1`
+exec "$@"
