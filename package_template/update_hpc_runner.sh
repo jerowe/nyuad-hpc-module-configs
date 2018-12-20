c96fd082d6ab284dfd166f05a0b16a4a192ee012
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-12-20 13:02:46 +0300

    updating before refactor

--- package_template/update_hpc_runner.sh
+++ package_template/update_hpc_runner.sh
@@ -5,6 +5,11 @@ module load gencore_biosails
 
 git clone -b feature/aws-batch https://github.com/biosails/HPC-Runner-Command
 cd HPC-Runner-Command
+
+sed -i.bak 's|#!/usr/bin/env perl|#!perl|g' script/hpcrunner.pl
+rm -rf script/hpcrunner.pl.bak
+
+HOME=/tmp cpanm --installdeps .
 cpanm .
 
 cd ..
